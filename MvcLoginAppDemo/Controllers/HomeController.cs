6b4640b5b22ba020afb6f489f7845c92bb73d316
Gaurav Saini <081gaurav@gmail.com>
2022-10-05 05:58:31 +0530

    upload

---
+++ MvcLoginAppDemo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcLoginAppDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserProfile objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DB_Entities db = new DB_Entities())
+                {
+                    var obj = db.UserProfiles.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserId.ToString();
+                        Session["UserName"] = obj.UserName.ToString();
+                        return RedirectToAction("Home");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult Home()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
